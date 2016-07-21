21843446d000cca796cd65f560b792c2e1956280
hgarud <hrishi.garud@gmail.com>
2016-07-21 18:37:49 +0530

    assignment number 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions described below output the inverse of a matrix.
+## But finding the inverse is a very costly aware in terms of the time complexity.
+## So buy using the concept of 'cache' we can improve the performance.
+## The first function uses a 'super assignment' operator, i.e. "<<-" 
+## to assign a value to a variable outside of the current environment.
 
-## Write a short comment describing this function
+
+## This function returns a "matrix" containing a variable and other functions.
+## It actually returns a list.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y){
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setInv <- function(inverse) inv <<- inverse
+        getInv <- function() inv
+        list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## This function actually returns the inverse of a matrix contructed 
+## by the above function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInv()
+        if(!is.null(inv)){
+                message("Getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setInv(inv)
+        inv
 }
